e3bd84bdf78accfbbc7ee4e21f2166d75737670b
baa76 <dev@brynroberts.net>
2018-02-17 07:26:38 +1100

    Define structure of JS and add inputs.

---
+++ resources/js/main.js
@@ -0,0 +1,21 @@
+// Inputs
+const startDate = new Date("December 4, 2017");
+const currentDate = new Date();
+const weeklyRepayment = 25;
+const loanPrincipal = 5500;
+
+// Outputs
+// Current amount owing
+
+
+// Number of payments since loan start
+
+
+// Date of most recent payment
+
+
+// List of all payments to current date
+
+
+
+// Date of last repayment
