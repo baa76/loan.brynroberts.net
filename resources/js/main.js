2e53c3161c61ed34be7623169678091e62116446
baa76 <dev@brynroberts.net>
2018-02-19 05:13:21 +1100

    Add last of the initial js elements.

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,19 +1,14 @@
 // Inputs
 const startDate = new Date("December 4, 2017");
 const startDateMs = startDate.getTime();
-console.log(startDateMs);
-console.log(startDate);
 const currentDate = new Date();
 const oneWeekMs = (1000*60*60*24*7);
 const weeklyRepayment = 25;
 const loanPrincipal = 5500;
 
-
-
-
 // Outputs
 const weeksDifference = Math.abs(startDate - currentDate);
-console.log(weeksDifference);
+// console.log(weeksDifference);
 
 // Number of payments(weeks) since loan start
 const paymentsSinceStart =  Math.floor(weeksDifference / oneWeekMs);
@@ -25,26 +20,24 @@ console.log(`Total $ paid to date: $${totalPaid}`);
 
 // Date of most recent payment
 const findLastPayment = (startDateMs + weeksDifference);
-console.log(`Find Last Payment: ${findLastPayment}`);
+// console.log(`Find Last Payment: ${findLastPayment}`);
 const roundedWeeksMs = startDateMs + (oneWeekMs * paymentsSinceStart);
 const dateOfMostRecentPayment = new Date(roundedWeeksMs).toDateString();
-console.log(dateOfMostRecentPayment);
+console.log(`Date of most recent payment: ${dateOfMostRecentPayment}`);
 
 // Current amount owing
 const calcCurrentAmountOwing = loanPrincipal - (weeklyRepayment * paymentsSinceStart);
 console.log(`Current Outstanding Loan Amount: $${calcCurrentAmountOwing}`);
 
-
-
-
 // List of all payments to current date
-
-
+for(i=0; i<=paymentsSinceStart; i++) {
+   const allPaymentsToDate =  new Date(startDateMs + (i * oneWeekMs)).toDateString();
+   console.log(allPaymentsToDate);
+}
 
 // Date of last repayment
-
 const getDateOfLastPayment = ((loanPrincipal / weeklyRepayment) * oneWeekMs) + startDateMs;
-console.log(getDateOfLastPayment);
+// console.log(getDateOfLastPayment);
 const dateOfLastPayment = new Date(getDateOfLastPayment).toDateString();
 console.log(dateOfLastPayment);
 
