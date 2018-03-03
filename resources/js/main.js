83d95555a3638c807ff9b382aac9f12ef7254ef1
baa76 <dev@brynroberts.net>
2018-03-03 17:01:34 +1100

    add js outputs - moving to bootstrap edition ---> printreadypdf.net bs

--- resources/js/main.js
+++ resources/js/main.js
@@ -6,39 +6,40 @@ const oneWeekMs = (1000*60*60*24*7);
 const weeklyRepayment = 25;
 const loanPrincipal = 5500;
 
-// Outputs
+
+// Workings
 const weeksDifference = Math.abs(startDate - currentDate);
-// console.log(weeksDifference);
+const findLastPayment = (startDateMs + weeksDifference);
+
+// Outputs
 
 // Number of payments(weeks) since loan start
+const startDateText = startDate.toDateString();
 const paymentsSinceStart =  Math.floor(weeksDifference / oneWeekMs);
-
-// const paymentsSinceStart = weeksDifference(startDate, currentDate);
 const totalPaid = paymentsSinceStart * weeklyRepayment;
-console.log(`Number of payments to date: ${paymentsSinceStart}`);
-console.log(`Total $ paid to date: $${totalPaid}`);
 
 // Date of most recent payment
-const findLastPayment = (startDateMs + weeksDifference);
-// console.log(`Find Last Payment: ${findLastPayment}`);
 const roundedWeeksMs = startDateMs + (oneWeekMs * paymentsSinceStart);
 const dateOfMostRecentPayment = new Date(roundedWeeksMs).toDateString();
-console.log(`Date of most recent payment: ${dateOfMostRecentPayment}`);
 
 // Current amount owing
 const calcCurrentAmountOwing = loanPrincipal - (weeklyRepayment * paymentsSinceStart);
-console.log(`Current Outstanding Loan Amount: $${calcCurrentAmountOwing}`);
 
 // List of all payments to current date
 for(i=0; i<=paymentsSinceStart; i++) {
    const allPaymentsToDate =  new Date(startDateMs + (i * oneWeekMs)).toDateString();
-   console.log(allPaymentsToDate);
+//    return allPaymentsToDate;
 }
 
 // Date of last repayment
 const getDateOfLastPayment = ((loanPrincipal / weeklyRepayment) * oneWeekMs) + startDateMs;
-// console.log(getDateOfLastPayment);
 const dateOfLastPayment = new Date(getDateOfLastPayment).toDateString();
-console.log(dateOfLastPayment);
 
-
+// Render
+document.getElementById("principal").innerHTML = `Principal: $${ loanPrincipal }`;
+document.getElementById("repayment").innerHTML = `Weekly Repayment: $${ weeklyRepayment }`;
+document.getElementById("start-date").innerHTML = `Start date: ${ startDateText }`;
+document.getElementById("end-date").innerHTML = `End date: ${ dateOfLastPayment }`;
+document.getElementById("most-recent-payment-date").innerHTML = `Most recent payment: ${ dateOfMostRecentPayment }`;
+document.getElementById("current-owing").innerHTML = `Current outstanding: $${ calcCurrentAmountOwing }`;
+// document.getElementById("all-payments").innerHTML = `List of all payments ${ allPaymentsToDate }`;
